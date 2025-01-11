163466f890ea4fd49d1b0327c616e8acca8ab2d0
Kevin McDonnell <kevin@mcd79.com>
2025-01-11 09:53:26 +0000

    More integration

--- Mcd79MinimalTuringSite/api/src/functions/SubmitWord.ts
+++ Mcd79MinimalTuringSite/api/src/functions/SubmitWord.ts
@@ -1,4 +1,8 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, HttpRequest, HttpResponseInit, InvocationContext, output } from "@azure/functions";
+
+/*
+    This function will capture the word posted and store it as a json file in the storage account.
+*/
 
 export async function SubmitWord(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
@@ -11,5 +15,11 @@ export async function SubmitWord(request: HttpRequest, context: InvocationContex
 app.http('SubmitWord', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: SubmitWord
+    handler: SubmitWord,
+    extraOutputs: [{
+        name: 'outputBlob',
+        type: 'blob',
+        path: 'humanwords/{queueTrigger}-${Date.now()}-humanword.txt',
+        connection: 'HumanWordsStorageConnectionAppSetting',
+    }]
 });
